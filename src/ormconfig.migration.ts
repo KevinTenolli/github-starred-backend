6d9b5ebc7385dba2faf663f2c62b51a03a1388a4
Kevin Tenolli <tenollikevin@gmail.com>
2024-02-29 20:25:06 +0100

    fix: issues with migrations

--- src/ormconfig.migration.ts
+++ src/ormconfig.migration.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata'
 import { DataSource } from "typeorm"
+import dotenv from "dotenv"
+dotenv.config()
 
 const MigrationSource = new DataSource({
     type: 'postgres',
     host: process.env.POSTGRES_HOST_1,
-    port: parseInt(  '5452'),
-    username: 'testUser',
-    password: '1234',
-    database: 'testDb',
+    port: parseInt(process.env.POSTGRES_PORT_1 || '5432'),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     synchronize: false,
     logging: true,
     entities: ['src/entity/*.ts'],
